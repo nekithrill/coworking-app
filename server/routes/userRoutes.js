f1a0bc2d4844d01cc679f080084ebff58c391406
nekithrill <nemnogoznaal@gmail.com>
2024-02-02 14:57:38 +0200

    created routes for user and admin

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const UserController = require('../controllers/userController')
+
+// Роуты для пользователей
+router.post('/register', UserController.registerUser)
+router.post('/login', UserController.loginUser)
+
+router.get('/profile/:userId', UserController.getUserProfile)
+router.put('/profile/:userId', UserController.updateUserProfile)
+
+router.get('/workspaces', UserController.getAllWorkspaces)
+router.get('/workspaces/free', UserController.searchWorkspaces)
+router.post('/create-booking', UserController.createBooking)
+
+module.exports = router
