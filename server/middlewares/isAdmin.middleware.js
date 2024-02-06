5e908fa36124fc00a85a2d0696e932773a497e51
nekithrill <nemnogoznaal@gmail.com>
2024-02-06 19:06:29 +0200

    created middlewares for auth and admin status check

---
+++ server/middlewares/isAdmin.middleware.js
@@ -0,0 +1,11 @@
+const isAdminMiddleware = (req, res, next) => {
+	if (!req.user) {
+		return res.status(401).json({ message: 'Unauthorized' })
+	}
+	if (req.user.role !== 'admin') {
+		return res.status(403).json({ message: 'Forbidden' })
+	}
+	next()
+}
+
+module.exports = isAdminMiddleware
