e8b2c4a4c35c309340d908ac6e689aaa7968e119
nekithrill <nemnogoznaal@gmail.com>
2024-02-07 17:57:21 +0200

    added enum for some fields

--- server/models/workspace.model.js
+++ server/models/workspace.model.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose')
 const workspaceSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	capacity: { type: Number, required: true },
-	location: { type: String },
-	type: { type: String },
-	amenities: [{ type: String }]
+	location: { type: String, required: true },
+	type: {
+		type: String,
+		enum: ['Office', 'Co-Working', 'Meeting Room', 'Event Space'],
+		default: 'Office',
+		required: true
+	},
+
+	amenities: {
+		type: [String],
+		enum: ['Wi-Fi', 'Coffee', 'Printer', 'Parking', 'Kitchen'],
+		default: []
+	}
 })
 
 const Workspace = mongoose.model('Workspace', workspaceSchema)
