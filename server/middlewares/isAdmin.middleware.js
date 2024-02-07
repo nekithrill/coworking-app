34f6a07c5a0b896ddd2d7b68a665421ab8fdc3e4
nekithrill <nemnogoznaal@gmail.com>
2024-02-07 17:58:46 +0200

    unnecessary changes

--- server/middlewares/isAdmin.middleware.js
+++ server/middlewares/isAdmin.middleware.js
@@ -1,10 +1,12 @@
 const isAdminMiddleware = (req, res, next) => {
 	if (!req.user) {
-		return res.status(401).json({ message: 'Unauthorized' })
+		return res.status(401).json({ error: 'Unauthorized' })
 	}
+
 	if (req.user.role !== 'admin') {
-		return res.status(403).json({ message: 'Forbidden' })
+		return res.status(403).json({ error: 'Forbidden' })
 	}
+
 	next()
 }
 
