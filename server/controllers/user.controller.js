e0667987eb5ef263c7ffd3190da8630cbb4be609
nekithrill <nemnogoznaal@gmail.com>
2024-02-07 17:59:43 +0200

    added bcrypt for hashing pass, finished some common logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,19 +1,40 @@
 const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+const generateToken = require('../utils/generateToken')
 const { errorHandler } = require('../utils/errorHandler')
 
 // * User
 const registerUser = async (req, res) => {
 	try {
-		const newUser = await User.create(req.body)
+		const { password, ...userData } = req.body
+		const hashedPassword = await bcrypt.hash(password, 10)
+
+		const newUser = await User.create({ ...userData, password: hashedPassword })
+
 		res.status(201).json(newUser)
 	} catch (error) {
 		errorHandler(400, error, res)
-		// res.status(400).json({ message: error.message })
 	}
 }
 
-const loginUser = (req, res) => {
-	// Логика входа пользователя
+const loginUser = async (req, res) => {
+	try {
+		const { email, password } = req.body
+		const user = await User.findOne({ email })
+		if (!user) {
+			return errorHandler(401, 'Invalid credentials', res)
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password)
+		if (!isPasswordValid) {
+			return errorHandler(401, 'Invalid credentials', res)
+		}
+
+		const tokenInfo = generateToken(user._id)
+		res.status(200).json(tokenInfo)
+	} catch (error) {
+		return errorHandler(500, error, res)
+	}
 }
 
 const getAllUsers = async (req, res) => {
@@ -21,7 +42,7 @@ const getAllUsers = async (req, res) => {
 		const allUsers = await User.find()
 		res.status(200).json(allUsers)
 	} catch (error) {
-		res.status(400).json({ message: error.message })
+		errorHandler(400, error, res)
 	}
 }
 
@@ -29,11 +50,11 @@ const getUserById = async (req, res) => {
 	try {
 		const user = await User.findById(req.params.userId)
 		if (!user) {
-			return res.status(404).json({ message: 'User not found' })
+			return errorHandler(404, error, res)
 		}
 		res.status(200).json(user)
 	} catch (error) {
-		res.status(500).json({ message: error.message })
+		errorHandler(500, error, res)
 	}
 }
 
@@ -45,11 +66,11 @@ const updateUserById = async (req, res) => {
 			{ new: true }
 		)
 		if (!updatedUser) {
-			return res.status(404).json({ message: 'User not found' })
+			return errorHandler(404, error, res)
 		}
 		res.status(200).json(updatedUser)
 	} catch (error) {
-		res.status(500).json({ message: error.message })
+		errorHandler(500, error, res)
 	}
 }
 
@@ -57,11 +78,11 @@ const deleteUserById = async (req, res) => {
 	try {
 		const deletedUser = await User.findByIdAndDelete(req.params.userId)
 		if (!deletedUser) {
-			return res.status(404).json({ message: 'User not found' })
+			return errorHandler(404, error, res)
 		}
 		res.status(200).json({ message: 'User deleted successfully' })
 	} catch (error) {
-		res.status(500).json({ message: error.message })
+		errorHandler(500, error, res)
 	}
 }
 
