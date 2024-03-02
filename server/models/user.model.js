862f823f0e049abd5e03ce65a8ad814ec30d2681
nekithrill <nemnogoznaal@gmail.com>
2024-03-02 14:45:18 +0200

    deleted unnecessary fiedls

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	role: { type: String, enum: ['user', 'admin'], default: 'user' }
+	role: { type: String, enum: ['user', 'admin'], default: 'user' },
+	isActivated: { type: Boolean, default: false },
+	activationLink: { type: String }
 })
 
 const User = mongoose.model('User', userSchema)
