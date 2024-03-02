21d596370c7f869f7f7e4cd56af98534262a5f28
nekithrill <nemnogoznaal@gmail.com>
2024-03-02 14:43:33 +0200

    added workspaceId field

--- server/models/booking.model.js
+++ server/models/booking.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const bookingSchema = new mongoose.Schema({
 	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	workspaceId: {
+	workspace: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Workspace',
 		required: true
