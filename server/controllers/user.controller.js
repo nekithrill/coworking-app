d1753c6e7b8e413886981d28ff21106a588e292c
nekithrill <nemnogoznaal@gmail.com>
2024-03-24 16:42:29 +0200

    edited registration function, added validation

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,14 +1,18 @@
 const User = require('../models/user.model')
-const bcrypt = require('bcrypt')
 const userService = require('../services/user-service')
-const generateToken = require('../services/token-service')
+const { validationResult } = require('express-validator')
+const ApiError = require('../errors/api-error')
 
 const registerUser = async (req, res, next) => {
 	try {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return next(ApiError.BadRequestError('Validation error', errors.array()))
+		}
 		const { email, password } = req.body
 		const userData = await userService.registration(email, password)
 		res.cookie('refreshToken', userData.refreshToken, {
-			maxAge: 30, // ! INCREASE
+			maxAge: 30 * 24 * 60 * 60 * 1000, // ! 30D AGE
 			httpOnly: true
 		})
 		return res.json(userData)
@@ -21,7 +25,10 @@ const loginUser = async (req, res, next) => {
 	try {
 		const { email, password } = req.body
 		const userData = await userService.login(email, password)
-		res.cookie('refreshToken', userData.refreshToken, { maxAge: 30, httpOnly })
+		res.cookie('refreshToken', userData.refreshToken, {
+			maxAge: 30,
+			httpOnly: true
+		})
 		return res.json(userData)
 	} catch (error) {
 		next(error)
@@ -41,7 +48,7 @@ const logoutUser = async (req, res, next) => {
 
 const activateUser = async (req, res, next) => {
 	try {
-		const activationLink = req.params.link
+		const activationLink = req.params.activationLink
 		await userService.activate(activationLink)
 		return res.redirect(process.env.CLIENT_URL)
 	} catch (error) {
