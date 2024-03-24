03e5e096b031a3c2f8c5ef49549ad0376489ff2c
nekithrill <nemnogoznaal@gmail.com>
2024-03-24 16:43:06 +0200

    renamed names of static methods

--- server/errors/api-error.js
+++ server/errors/api-error.js
@@ -8,51 +8,51 @@ module.exports = class ApiError extends Error {
 		this.errors = errors
 	}
 
-	static badRequestError(message, errors = []) {
+	static BadRequestError(message, errors = []) {
 		return new ApiError(400, message, errors)
 	}
 
-	static unauthorizedError() {
+	static UnauthorizedError() {
 		return new ApiError(401, 'Authentication error')
 	}
 
-	static forbiddenError(message, errors = []) {
+	static ForbiddenError(message, errors = []) {
 		return new ApiError(403, message, errors)
 	}
 
-	static notFoundError(message, errors = []) {
+	static NotFoundError(message, errors = []) {
 		return new ApiError(404, message, errors)
 	}
 
-	static requestTimeoutError(message, errors = []) {
+	static RequestTimeoutError(message, errors = []) {
 		return new ApiError(408, message, errors)
 	}
 
-	static tooManyRequestsError(message, errors = []) {
+	static TooManyRequestsError(message, errors = []) {
 		return new ApiError(429, message, errors)
 	}
 
-	static internalServerError(message, errors = []) {
+	static InternalServerError(message, errors = []) {
 		return new ApiError(500, message, errors)
 	}
 
-	static notImplementedError(message, errors = []) {
+	static NotImplementedError(message, errors = []) {
 		return new ApiError(501, message, errors)
 	}
 
-	static badGatewayError(message, errors = []) {
+	static BadGatewayError(message, errors = []) {
 		return new ApiError(502, message, errors)
 	}
 
-	static serviceUnavailableError(message, errors = []) {
+	static ServiceUnavailableError(message, errors = []) {
 		return new ApiError(503, message, errors)
 	}
 
-	static gatewayTimeoutError(message, errors = []) {
+	static GatewayTimeoutError(message, errors = []) {
 		return new ApiError(504, message, errors)
 	}
 
-	static customError(statusCode, message, errors = []) {
+	static CustomError(statusCode, message, errors = []) {
 		return new ApiError(statusCode, message, errors)
 	}
 }
