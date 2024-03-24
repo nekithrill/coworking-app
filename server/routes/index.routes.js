aafde535e7a74a2f961df9c4a24e1fbd6cfd187d
nekithrill <nemnogoznaal@gmail.com>
2024-03-24 16:46:54 +0200

    added tariff routes to general route file

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -6,11 +6,13 @@ const bookingRouter = require('./booking.routes')
 const newsRouter = require('./news.routes')
 const userRouter = require('./user.routes')
 const workspaceRouter = require('./workspace.routes')
+const tariffRouter = require('./tariff.routes')
 
 Router.use('/ad', adRouter)
 Router.use('/booking', bookingRouter)
 Router.use('/news', newsRouter)
 Router.use('/user', userRouter)
 Router.use('/workspace', workspaceRouter)
+Router.use('/tariff', tariffRouter)
 
 module.exports = Router
