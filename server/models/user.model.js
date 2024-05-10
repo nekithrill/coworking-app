d94bd3a8e782e38f79639533e19fb576ae5e43f6
nekithrill <nemnogoznaal@gmail.com>
2024-05-10 16:28:31 +0300

    changed field role (added new roles)

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	role: { type: String, enum: ['user', 'admin'], default: 'user' },
+	role: { type: String, enum: ['user', 'admin', 'creator'], default: 'user' },
 	isActivated: { type: Boolean, default: false },
 	activationLink: { type: String }
 })
