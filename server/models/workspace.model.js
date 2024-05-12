171db2830c884c97dbe3d6702b99e26965a9e17d
nekithrill <nemnogoznaal@gmail.com>
2024-05-12 13:40:49 +0300

    removed unused fields, added new fields

--- server/models/workspace.model.js
+++ server/models/workspace.model.js
@@ -2,20 +2,22 @@ const mongoose = require('mongoose')
 
 const workspaceSchema = new mongoose.Schema({
 	name: { type: String, required: true },
+	description: { type: String, required: true },
+	images: [String],
 	capacity: { type: Number, required: true },
 	location: { type: String, required: true },
-	status: {
-		type: String,
-		enum: ['occupied', 'available'],
-		default: 'available'
-	},
+	coefficient: { type: Number, required: true },
 	type: {
 		type: String,
 		enum: ['Office', 'Co-Working', 'Meeting Room', 'Event Space'],
 		default: 'Office',
 		required: true
 	},
-
+	status: {
+		type: String,
+		enum: ['occupied', 'available'],
+		default: 'available'
+	},
 	amenities: {
 		type: [String],
 		enum: ['Wi-Fi', 'Coffee', 'Printer', 'Parking', 'Kitchen'],
