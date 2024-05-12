bfb9ee5c99569c4314503e88bde38fe2de8dfcfd
nekithrill <nemnogoznaal@gmail.com>
2024-05-12 13:52:33 +0300

    added more fields for details

--- server/models/booking.model.js
+++ server/models/booking.model.js
@@ -2,16 +2,27 @@ const mongoose = require('mongoose')
 
 const bookingSchema = new mongoose.Schema({
 	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	workspace: {
+	workspaceId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Workspace',
 		required: true
 	},
-	tariff: {
+	tariffId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Tariff',
 		required: true
-	}
+	},
+	workspaceName: { type: String },
+	tariffName: { type: String },
+	status: {
+		type: String,
+		enum: ['active', 'completed', 'cancelled', 'pending'],
+		default: 'pending'
+	},
+	startDate: { type: Date },
+	endDate: { type: Date },
+	paymentStatus: { type: String, enum: ['paid', 'unpaid'], default: 'unpaid' },
+	totalPrice: { type: Number }
 })
 
 const Booking = mongoose.model('Booking', bookingSchema)
