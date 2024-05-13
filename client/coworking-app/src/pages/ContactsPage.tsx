096082b8488105f2ff2894bfe9f38ce14aa68a52
nekithrill <nemnogoznaal@gmail.com>
2024-05-13 22:32:36 +0300

    created templates for all pages

---
+++ client/coworking-app/src/pages/ContactsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ContactsPage: React.FC = () => {
+	return <div></div>
+}
+
+export default ContactsPage
---
+++ client/coworking-app/src/pages/TariffsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TariffsPage: React.FC = () => {
+	return <div></div>
+}
+
+export default TariffsPage
