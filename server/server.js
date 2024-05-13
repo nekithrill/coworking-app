4c4b210c7673affb550324feb2c60e2d732e32f0
nekithrill <nemnogoznaal@gmail.com>
2024-05-13 19:02:26 +0300

    added cors rules for client side

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true
+	})
+)
 app.use('/api', Router)
 app.use(errorHandler)
 
