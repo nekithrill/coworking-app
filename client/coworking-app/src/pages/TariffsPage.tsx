0fb3b9d9659eb4bf9079a6bbe656d95e8694d395
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:12:47 +0300

    tariffs page created and stylized

--- client/coworking-app/src/pages/TariffsPage.tsx
+++ client/coworking-app/src/pages/TariffsPage.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import TariffCard from '../components/tariffs/TariffCard'
+import { ITariff } from '../models/entities/ITariff'
+import TariffService from '../services/TariffService'
+import '../styles/layout/_tariffsPage.scss'
 
-const TariffsPage: React.FC = () => {
-	return <div></div>
+const TariffsPage = () => {
+	const [data, setData] = useState<ITariff[]>([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true)
+			try {
+				const response = await TariffService.getAllTariffs()
+				setData(response.data)
+			} catch (error) {
+				console.error('Error fetching tariffs:', error)
+			} finally {
+				setLoading(false)
+			}
+		}
+		fetchData()
+	}, [])
+
+	return (
+		<div className='tariffs'>
+			<div className='tariffs__header'>
+				<h1>Tariffs</h1>
+				<p>On this page you can find all available tariffs.</p>
+			</div>
+
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<div className='tariffs__content'>
+					{data.map(item => (
+						<TariffCard key={item._id} data={item} />
+					))}
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default TariffsPage
