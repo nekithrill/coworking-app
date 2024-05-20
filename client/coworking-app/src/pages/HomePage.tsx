7fc7489395fe9bc6574836b4cd05287df484771b
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:11:29 +0300

    home page created and stylized

--- client/coworking-app/src/pages/HomePage.tsx
+++ client/coworking-app/src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import '../styles/layout/_homePage.scss'
 
 const HomePage: React.FC = () => {
-	return <div></div>
+	return (
+		<div className='home'>
+			<div className='home__hero'>
+				<h1>Home title</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint
+					omnis. Veniam, mollitia amet animi necessitatibus suscipit ipsum
+					architecto, qui magnam ad laudantium nesciunt officiis.
+				</p>
+			</div>
+
+			<button className='bookingButton'>make booking</button>
+		</div>
+	)
 }
 
 export default HomePage
