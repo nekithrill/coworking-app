9acd966780d41a695a129f97293ac371e757598a
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:12:12 +0300

    error page (not found) created

---
+++ client/coworking-app/src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/layout/_notFoundPage.scss'
+
+const NotFoundPage: React.FC = () => {
+	return (
+		<div className='error_page'>
+			<h1>Error 404 - Page not found</h1>
+		</div>
+	)
+}
+
+export default NotFoundPage
