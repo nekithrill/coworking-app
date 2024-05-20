efcfe15a5068b069d1fc19cb7b3155490c3e130b
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:07:16 +0300

    init

---
+++ client/coworking-app/src/components/auth/AuthModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/components/_authModal.scss'
+
+const AuthModal: React.FC = () => {
+	const handleLogin = () => {}
+
+	const handleSignUp = () => {}
+
+	return <div></div>
+}
+
+export default AuthModal
