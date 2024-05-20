ab9151e9626ee087bf59ccbaf6e526445ff669fb
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:10:15 +0300

    workspace card created and stylized

---
+++ client/coworking-app/src/components/workspaces/WorkspaceCard.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { IWorkspace } from '../../models/entities/IWorkspace'
+import '../../styles/components/_workspaceCard.scss'
+
+interface WorkspaceCardProps {
+	data: IWorkspace
+}
+
+const WorkspaceCard: React.FC<WorkspaceCardProps> = ({ data }) => {
+	const [currentSlide, setCurrentSlide] = useState(0)
+
+	const nextSlide = () => {
+		setCurrentSlide(prevSlide =>
+			prevSlide === (data.images?.length ?? 0) - 1 ? 0 : prevSlide + 1
+		)
+	}
+
+	const prevSlide = () => {
+		setCurrentSlide(prevSlide =>
+			prevSlide === 0 ? (data.images?.length ?? 0) - 1 : prevSlide - 1
+		)
+	}
+
+	const sliderStyle = {
+		'--currentSlide': currentSlide
+	} as React.CSSProperties
+
+	return (
+		<div className='workspace-card'>
+			<div className='workspace-card__header'>
+				<h2>{data.name}</h2>
+				<p>{data.description}</p>
+			</div>
+
+			<div className='workspace-card__sidebar'>
+				<h2>Properties</h2>
+				<p>Capacity - {data.capacity}</p>
+				<p>Location - {data.location}</p>
+				<p>Type - {data.type}</p>
+				<p>Status - {data.status}</p>
+				{data.amenities && (
+					<div className='amenities'>
+						<h4>Amenities:</h4>
+						<ul>
+							{data.amenities.map((amenity, index) => (
+								<li key={index}>{amenity}</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
+
+			<div className='workspace-card__images'>
+				{data.images ? (
+					<div className='workspace-card__images_slider' style={sliderStyle}>
+						{data.images.map((image, index) => (
+							<div
+								key={index}
+								className={`slide ${index === currentSlide ? 'active' : ''}`}
+							>
+								<img src={image} alt={`Workspace image ${index + 1}`} />
+							</div>
+						))}
+					</div>
+				) : (
+					<p>No images available</p>
+				)}
+				{data.images && data.images.length > 1 && (
+					<div>
+						<button className='prevButton' onClick={prevSlide}>
+							Prev
+						</button>
+						<button className='nextButton' onClick={nextSlide}>
+							Next
+						</button>
+					</div>
+				)}
+			</div>
+		</div>
+	)
+}
+
+export default WorkspaceCard
