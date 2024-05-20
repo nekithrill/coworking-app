cba93ebac342b92f0699236ea9ab42276a2ad84d
nekithrill <nemnogoznaal@gmail.com>
2024-05-20 20:11:05 +0300

    about page created and stylized

--- client/coworking-app/src/pages/AboutPage.tsx
+++ client/coworking-app/src/pages/AboutPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../styles/layout/_aboutPage.scss'
 
 const AboutPage: React.FC = () => {
-	return <div></div>
+	return (
+		<div className='about'>
+			<h1>About</h1>
+		</div>
+	)
 }
 
 export default AboutPage
