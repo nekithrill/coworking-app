dfaee8cbee83b26a74b279f7162bc79c20ef4254
nekithrill <nemnogoznaal@gmail.com>
2024-06-11 11:03:13 +0300

    Error page component

--- client/coworking-app/src/pages/NotFoundPage.tsx
+++ client/coworking-app/src/pages/NotFoundPage.tsx
@@ -4,7 +4,9 @@ import '../styles/layout/_notFoundPage.scss'
 const NotFoundPage: React.FC = () => {
 	return (
 		<div className='error_page'>
-			<h1>Error 404 - Page not found</h1>
+			<h1>
+				Error 404 <span>Page not found</span>
+			</h1>
 		</div>
 	)
 }
