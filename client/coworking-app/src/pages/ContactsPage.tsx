8b38dc38edd66cab2800b750b6f8e5bf9aee038d
nekithrill <nemnogoznaal@gmail.com>
2024-06-11 11:04:18 +0300

    added icons, edited structure

--- client/coworking-app/src/pages/ContactsPage.tsx
+++ client/coworking-app/src/pages/ContactsPage.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import InstIcon from '../components/icons/InstIcon'
+import LnkdinIcon from '../components/icons/LnkdinIcon'
+import LocationIcon from '../components/icons/LocationIcon'
+import MailIcon from '../components/icons/MailIcon'
+import PhoneIcon from '../components/icons/PhoneIcon'
+import TgIcon from '../components/icons/TgIcon'
 import '../styles/layout/_contactsPage.scss'
 
 const ContactsPage: React.FC = () => {
@@ -16,17 +22,38 @@ const ContactsPage: React.FC = () => {
 				<div className='contacts__info'>
 					<div className='contacts__info-block'>
 						<h2>Contact Information</h2>
-						<p>ADDR: 1234 Street Name, City, Country</p>
-						<p>PHONE: +123 456 7890</p>
-						<p>MAIL: nativesync.confirmation@gmail.com</p>
+						<div className='contacts__info-block-item'>
+							<LocationIcon color='var(--clr-contacts-icon)' />
+							<p>1234 Street Name, City, Country</p>
+						</div>
+
+						<div className='contacts__info-block-item'>
+							<PhoneIcon color='var(--clr-contacts-icon)' />
+							<p>+38 (099) 123 45 67</p>
+						</div>
+
+						<div className='contacts__info-block-item'>
+							<MailIcon color='var(--clr-contacts-icon)' />
+							<p>nativesync.confirmation@gmail.com</p>
+						</div>
 					</div>
 
 					<div className='contacts__info-block'>
 						<h2>Social media</h2>
-						<p>TG: @exampleuser</p>
-						<p>INST: @exampleuser</p>
-						<p>LNKDIN: @exampleuser</p>
-						<p></p>
+						<div className='contacts__info-block-item'>
+							<TgIcon color='var(--clr-contacts-icon)' />
+							<p>@nativesync_coworking</p>
+						</div>
+
+						<div className='contacts__info-block-item'>
+							<InstIcon color='var(--clr-contacts-icon)' />
+							<p>@nativesync_coworking</p>
+						</div>
+
+						<div className='contacts__info-block-item'>
+							<LnkdinIcon color='var(--clr-contacts-icon)' />
+							<p>@nativesync_coworking</p>
+						</div>
 					</div>
 				</div>
 
@@ -34,34 +61,34 @@ const ContactsPage: React.FC = () => {
 					<h2>Contact Us</h2>
 					<form>
 						<div className='contacts__form_group'>
-							<label htmlFor='email'>Email</label>
 							<input
 								type='email'
 								id='email'
 								name='email'
-								placeholder='Your email'
+								autoComplete='off'
 								required
 							/>
+							<label htmlFor='email'>Email</label>
 						</div>
 						<div className='contacts__form_group'>
-							<label htmlFor='phone'>Phone</label>
 							<input
 								type='tel'
 								id='phone'
 								name='phone'
 								pattern='[+]{0,1}[0-9]{1,15}'
-								placeholder='+1234567890'
+								autoComplete='off'
 								required
 							/>
+							<label htmlFor='phone'>Phone number</label>
 						</div>
 						<div className='contacts__form_group'>
-							<label htmlFor='message'>Message</label>
 							<textarea
 								id='message'
 								name='message'
-								placeholder='Write your message here'
+								autoComplete='off'
 								required
 							></textarea>
+							<label htmlFor='message'>Message</label>
 						</div>
 						<button type='submit'>Send</button>
 					</form>
@@ -71,7 +98,7 @@ const ContactsPage: React.FC = () => {
 					<h2>Our Location</h2>
 					<iframe
 						title='Our Location'
-						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.247940081172!2d144.9537363157776!3d-37.81627984209313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf57778f5d0b4e9f!2sFlinders%20St%20Station!5e0!3m2!1sen!2sau!4v1625572326320!5m2!1sen!2sau'
+						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d796.4992891259982!2d35.18153507332958!3d47.818403874056955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc5e0a7b28cc41%3A0x701aee926c1313c8!2sZaporizhia%20National%20University!5e0!3m2!1sru!2sua!4v1718090445955!5m2!1sru!2sua'
 						width='100%'
 						height='400'
 						style={{ border: 0 }}
