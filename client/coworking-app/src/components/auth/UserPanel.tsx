2a0e31a7e98bae1e30ed93ae3604c2d51a3f2d1e
nekithrill <nemnogoznaal@gmail.com>
2024-06-16 12:27:23 +0300

    user panel component init

--- client/coworking-app/src/components/auth/UserPanel.tsx
+++ client/coworking-app/src/components/auth/UserPanel.tsx
@@ -1,23 +1,59 @@
-import React, { useState } from 'react'
-import { IUser } from '../../models/entities/IUser'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../main'
+import BookingService from '../../services/BookingService'
 import '../../styles/components/_userPanel.scss'
+import ExpandIcon from '../icons/ExpandIcon'
 
-type UserPanelProps = {
-	user: IUser
-	onLogout: () => void
-}
-
-const UserPanel: React.FC<UserPanelProps> = ({ user, onLogout }) => {
+const UserPanel: React.FC = () => {
+	const { store } = useContext(AuthContext)
 	const [isExpanded, setIsExpanded] = useState(false)
 
+	const handleLogout = async () => {
+		try {
+			await store.logout()
+			// Переадресация пользователя на страницу входа после успешного выхода
+			// Можно использовать useHistory из react-router-dom для навигации
+			// например: history.push('/login');
+			alert('Logout successful')
+		} catch (error) {
+			console.error('Logout error:', error)
+		}
+	}
+
+	const handleViewBookingHistory = async () => {
+		try {
+			const response = await BookingService.viewBookingHistory()
+			console.log('Booking history:', response.data)
+		} catch (error) {
+			console.error('Error fetching booking history:', error)
+		}
+	}
+
+	useEffect(() => {
+		if (!store.user) {
+			setIsExpanded(false)
+		}
+	}, [store.user])
+
 	return (
-		<div className='user-panel'>
-			<div className='user-info' onClick={() => setIsExpanded(!isExpanded)}>
-				<span>{user.email}</span>
+		<div className='user__panel'>
+			<div
+				className='user__panel_info'
+				onClick={() => setIsExpanded(!isExpanded)}
+			>
+				<div className='email'>
+					<p>{store.user?.email}</p>{' '}
+					<ExpandIcon color='var(--auth-button-text)' />
+				</div>
 				{isExpanded && (
-					<div className='user-details'>
-						<p>Email: {user.email}</p>
-						<button onClick={onLogout}>Logout</button>
+					<div className='user__panel_info-details'>
+						{/* <p>Id: {store.user?._id}</p> */}
+						<p>Role: {store.user?.role}</p>
+						{/* <p>Activated: {store.user?.isActivated}</p> */}
+						<button onClick={handleLogout}>Logout</button>
+						<button onClick={handleViewBookingHistory}>
+							View Booking History
+						</button>
 					</div>
 				)}
 			</div>
