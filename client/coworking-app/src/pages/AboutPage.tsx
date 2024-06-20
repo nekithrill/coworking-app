b7b49e59b74869aac635e1f1d116e7f78e6c97a4
nekithrill <nemnogoznaal@gmail.com>
2024-06-20 15:51:36 +0300

    edited text of benefits

--- client/coworking-app/src/pages/AboutPage.tsx
+++ client/coworking-app/src/pages/AboutPage.tsx
@@ -11,57 +11,59 @@ const AboutPage: React.FC = () => {
 
 			<div className='about__benefits'>
 				<div className='about__benefits_card'>
-					<img src='/placeholder.jpg' alt='IMG' />
+					<img src='/infrastructure.webp' alt='IMG-01' />
 					<div className='about__benefits_card_content'>
 						<p className='card-number'>01</p>
-						<h2>First benefit</h2>
+						<h2>Modern amenities and infrastructure</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-							id iste quisquam libero veritatis praesentium numquam repellat
-							corporis dicta voluptatem illum, eius quaerat unde esse veniam
-							sequi aut iure fugit labore excepturi deserunt.
+							Our coworking space is equipped with all necessary amenities,
+							including high-speed Wi-Fi, printers, coffee stations, parking,
+							and a kitchen. This allows residents to work comfortably and
+							productively without leaving the building.
 						</p>
 					</div>
 				</div>
 
 				<div className='about__benefits_card'>
-					<img src='/placeholder.jpg' alt='IMG' />
+					<img src='/environment.webp' alt='IMG-02' />
 					<div className='about__benefits_card_content'>
 						<p className='card-number'>02</p>
-						<h2>Second benefit</h2>
+						<h2>Inspiring and productive environment</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-							id iste quisquam libero veritatis praesentium numquam repellat
-							corporis dicta voluptatem illum, eius quaerat unde esse veniam
-							sequi aut iure fugit labore excepturi deserunt.
+							Our coworking space is designed with modern interior trends in
+							mind, promoting creativity and productivity. Bright and ergonomic
+							work areas, cozy relaxation spots, and modern conference rooms
+							create an atmosphere conducive to effective work and interaction.
 						</p>
 					</div>
 				</div>
 
 				<div className='about__benefits_card'>
-					<img src='/placeholder.jpg' alt='IMG' />
+					<img src='/flexibility.webp' alt='IMG-03' />
 					<div className='about__benefits_card_content'>
 						<p className='card-number'>03</p>
-						<h2>Third benefit</h2>
+						<h2>Flexibility and accessibility</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-							id iste quisquam libero veritatis praesentium numquam repellat
-							corporis dicta voluptatem illum, eius quaerat unde esse veniam
-							sequi aut iure fugit labore excepturi deserunt.
+							Our coworking space offers various rental options, from hourly to
+							long-term, allowing residents to choose the most suitable option
+							for their needs. This is especially convenient for freelancers,
+							startups, and small teams who need flexibility in office space
+							usage.
 						</p>
 					</div>
 				</div>
 
 				<div className='about__benefits_card'>
-					<img src='/placeholder.jpg' alt='IMG' />
+					<img src='/community.webp' alt='IMG-04' />
 					<div className='about__benefits_card_content'>
 						<p className='card-number'>04</p>
-						<h2>Fourth benefit</h2>
+						<h2>Networking opportunities and community</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-							id iste quisquam libero veritatis praesentium numquam repellat
-							corporis dicta voluptatem illum, eius quaerat unde esse veniam
-							sequi aut iure fugit labore excepturi deserunt.
+							Our coworking space actively fosters a professional community by
+							organizing regular events, workshops, and networking meetings.
+							This is a great opportunity for residents to establish new
+							business contacts, share experiences, and find partners for joint
+							projects.
 						</p>
 					</div>
 				</div>
